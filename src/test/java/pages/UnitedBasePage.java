36237450c5a994bfbe230c89c5fd6f5ebc8c932f
ReginaIulgut <reginaiulgut@gmail.com>
2023-01-30 16:50:12 -0600

    added project

---
+++ src/test/java/pages/UnitedBasePage.java
@@ -0,0 +1,77 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.util.List;
+
+public class UnitedBasePage {
+
+    public UnitedBasePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(css = "ul[class='app-components-GlobalHeader-globalHeader__globalBottomNav--2aHvD app-components-GlobalHeader-globalHeader__hpBottomNav--3SjnI']")
+    public List<WebElement> mainMenuNavigation;
+
+    @FindBy(css = "ul[class='app-components-BookTravel-bookTravel__travelNav--3RNBj']")
+    public List<WebElement> bookTravelMenuNavigation;
+
+    @FindBy(css = "div[class*='radioContainer'] label")
+    public List<WebElement> radioButtons;
+
+    @FindBy(css = "div[class*='radioContainer'] input")
+    public List<WebElement> radioButtonsInput;
+
+    @FindBy(css = "div[class*='awardTravelContainer'] label")
+    public WebElement bookWithMilesCheckboxLabel;
+
+    @FindBy(css = "div[class*='awardTravelContainer'] input")
+    public WebElement bookWithMilesCheckboxInput;
+
+    @FindBy(css = "div[class*='flexibleDatesContainer'] label")
+    public WebElement flexibleDatesLabel;
+
+    @FindBy(css = "div[class*='flexibleDatesContainer'] input")
+    public WebElement flexibleDatesInput;
+
+
+    @FindBy(id ="bookFlightOriginInput")
+    public WebElement flightOriginLocation;
+
+    @FindBy(id = "bookFlightDestinationInput")
+    public WebElement finalDestinationLocation;
+
+    @FindBy(id = "DepartDate")
+    public WebElement departureDate;
+
+    @FindBy(css = "button[aria-labelledby='stuff']")
+    public WebElement passengerSelectButton;
+
+    @FindBy(css = "button[aria-label='Substract one Adult']")
+    public WebElement addAdultPassenger;
+
+    @FindBy(css = "div[class='app-components-ListBox-ListBox__listbox--2-kSS'")
+    public WebElement classDropdown;
+
+    @FindBy(id = "cabinType")
+    public List<WebElement> cabinType;
+
+    public void clickOnCabinTypeOption(String optionText){
+        for(WebElement element : cabinType){
+            if(element.getText().equals(optionText)){
+                element.click();
+                break;
+            }
+        }
+    }
+
+    @FindBy(css = "button[class*='bookFlightForm__findFlightBtn']")
+    public WebElement findFlightsButton;
+
+
+
+
+}
